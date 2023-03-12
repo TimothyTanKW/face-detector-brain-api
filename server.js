67fcb6798f915db23001b364f71eb4f03f88da71
TimothyTanKW <54305028+TimothyTanKW@users.noreply.github.com>
2023-03-12 13:37:14 +0800

    new update 12/3

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = knex({
     connection: {
         host: '127.0.0.1',
         user: 'postgres',
-        password: '',
+        password: 'something123',
         database: 'face-detector'
     }
 });
